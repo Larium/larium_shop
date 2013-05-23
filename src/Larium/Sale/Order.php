832d66380c2bbf35e30b377b1aedcff0f8c463c6
Andreas Kollaros <andreas.kollaros@gmail.com>
2013-05-23 16:17:46 +0300

    start implement Order class

--- src/Larium/Sale/Order.php
+++ src/Larium/Sale/Order.php
@@ -4,7 +4,36 @@
 
 namespace Larium\Sale;
 
-class Order 
+class Order implements OrderInterface 
 {
+    private $status;
 
+    /**
+     * {@inheritdoc}
+     */   
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addItem(OrderItemInterface $item)
+    {
+        $this->getItems()->add($item);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeItem(OrderItemInterface $item)
+    {
+        $this->getItems()->remove($item);
+    }
+    
+    public function getItems()
+    {
+        return $this->getOrderItems();
+    }
 }
