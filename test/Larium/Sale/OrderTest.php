55259207a01ff3dc3fbcbe383e8319c53c24dd55
Andreas Kollaros <andreas.kollaros@gmail.com>
2013-06-07 17:09:28 +0300

    reset order test

--- test/Larium/Sale/OrderTest.php
+++ test/Larium/Sale/OrderTest.php
@@ -17,22 +17,5 @@ class OrderTest extends \PHPUnit_Framework_TestCase
     public function testOrder()
     {
         $cart = new Cart();
-
-        $p1 = $this->loader->instanceFor('Larium\\Store\\Product', 'product_1');
-        $p2 = $this->loader->instanceFor('Larium\\Store\\Product', 'product_2');
-        $p3 = $this->loader->instanceFor('Larium\\Store\\Product', 'product_3');
-        
-        $b = $this->loader->instanceFor('Larium\\Payment\\BillingMethod', 'cash');
-
-        $item1 = $cart->addProduct($p1);
-        
-        $item2 = $cart->addProduct($p2);
-
-        $this->assertEquals(18, $cart->getOrder()->getTotalAmount());
-
-        //$cart->removeItem($item2);
-
-        $cart->addBillingMethod($b);
-        print_r($cart);
     } 
 }
