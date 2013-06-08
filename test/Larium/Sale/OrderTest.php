51fdfabe7a6ab56c564d4dd02d28c6a30fbcf1be
Andreas Kollaros <andreas.kollaros@gmail.com>
2013-06-08 14:09:19 +0300

    fix fixtures and test

--- test/Larium/Sale/OrderTest.php
+++ test/Larium/Sale/OrderTest.php
@@ -14,8 +14,46 @@ class OrderTest extends \PHPUnit_Framework_TestCase
         $this->loader = new \FixtureLoader();
     }
 
-    public function testOrder()
+    public function testCartAddingItems()
     {
         $cart = new Cart();
-    } 
+
+        $product1 = $this->getProduct('product_1');
+        $variant1 = $product1->getDefaultVariant();
+        $item1 = $cart->addItem($variant1);
+
+        $this->assertEquals(1, $cart->getItemsCount());
+
+        $product2 = $this->getProduct('product_2');
+        $variant2 = $product2->getDefaultVariant();
+        $item2 = $cart->addItem($variant2);
+        
+        $this->assertEquals(2, $cart->getTotalQuantity());
+
+        $this->assertEquals(21, $cart->getOrder()->getTotalAmount());
+
+    }
+
+    public function testCartAddSameVariant()
+    {
+        $cart = new Cart();
+
+        $product = $this->getProduct('product_1');
+        $variant = $product->getDefaultVariant();
+
+        $cart->addItem($variant);
+        $cart->addItem($variant);
+
+        $this->assertEquals(1, $cart->getItemsCount());
+        $this->assertEquals(2, $cart->getOrder()->getTotalQuantity());
+    }
+
+    private function getProduct($id)
+    {
+        $data = $this->loader->getData();
+        
+        $mapping = include __DIR__ . '/../../data_mapping.conf.php';
+        $hydrator = new \Hydrator('Larium\\Store\\Product', $mapping);
+        return $hydrator->hydrate($data[$id]);
+    }
 }
