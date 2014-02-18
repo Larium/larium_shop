0de0655a11cf1095fd12e35f670761f58fbbecbb
Andreas Kollaros <andreas.kollaros@gmail.com>
2014-02-18 10:38:52 +0200

    fix autoload classes

--- test/init.php
+++ test/init.php
@@ -1,8 +1,5 @@
 <?php
-require_once 'SplClassLoader.php';
-$l = new SplClassLoader('Larium', __DIR__ . '/../src');
-$l->register();
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/FixtureLoader.php';
 require_once __DIR__ . '/Hydrator.php';
 require_once __DIR__ . '/Larium/RedirectProvider.php';
-require_once __DIR__ . '/../vendor/autoload.php';
