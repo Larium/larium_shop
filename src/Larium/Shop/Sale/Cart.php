6468e71d3d9f0ad0b9a7f754b76be45e0a43ed81
akDeveloper <andreas.kollaros@gmail.com>
2015-03-17 15:00:20 +0200

    remove state machine config from Cart class.

--- src/Larium/Shop/Sale/Cart.php
+++ src/Larium/Shop/Sale/Cart.php
@@ -201,53 +201,9 @@ class Cart implements CartInterface
 
     protected function initialize_state_machine()
     {
-        $states = [
-            Order::CART       => ['type' => State::TYPE_INITIAL, 'properties' => []],
-            Order::CHECKOUT   => ['type' => State::TYPE_NORMAL, 'properties' => []],
-            Order::PARTIAL_PAID => ['type' => State::TYPE_NORMAL, 'properties' => []],
-            Order::PAID       => ['type' => State::TYPE_NORMAL, 'properties' => []],
-            Order::PROCESSING => ['type' => State::TYPE_NORMAL, 'properties' => []],
-            Order::SENT       => ['type' => State::TYPE_NORMAL, 'properties' => []],
-            Order::CANCELLED  => ['type' => State::TYPE_FINAL, 'properties' => []],
-            Order::DELIVERED  => ['type' => State::TYPE_FINAL, 'properties' => []],
-            Order::RETURNED   => ['type' => State::TYPE_FINAL, 'properties' => []],
-        ];
+        $config = include __DIR__ . '/../../../config/cart_finite_state.php';
 
-        $transitions = [
-            'checkout'    => ['from'=>[Order::CART], 'to' => Order::CHECKOUT],
-            'partial_pay' => ['from'=>[Order::PAID, Order::PARTIAL_PAID], 'to' => Order::PARTIAL_PAID],
-            'pay'         => ['from'=>[Order::CHECKOUT, Order::PARTIAL_PAID], 'to' => Order::PAID],
-            'process'     => ['from'=>[Order::PAID], 'to' => Order::PROCESSING],
-            'send'        => ['from'=>[Order::PROCESSING], 'to' => Order::SENT],
-            'deliver'     => ['from'=>[Order::SENT],'to' => Order::DELIVERED],
-            'return'      => ['from'=>[Order::SENT], 'to' => Order::RETURNED],
-            'cancel'      => ['from'=>[Order::PAID, Order::PROCESSING], 'to' => Order::CANCELLED],
-            'retry'       => ['from'=>[Order::CANCELLED], 'to' => Order::CHECKOUT],
-        ];
-
-        $callbacks = [
-            'after' => [
-                [
-                    'from' => [Order::CHECKOUT, Order::PARTIAL_PAID],
-                    'to'   => Order::PAID,
-                    'do'   => [$this->order, 'processPayments']
-                ],
-                [
-                    'from' => [Order::CHECKOUT, Order::PARTIAL_PAID],
-                    'to'   => Order::PAID,
-                    'do'   => [$this, 'rollbackPayment']
-                ]
-            ]
-        ];
-
-        $loader = new ArrayLoader(
-            [
-                'class' => get_class($this->order),
-                'states' => $states,
-                'transitions' => $transitions,
-                'callbacks' => $callbacks
-            ]
-        );
+        $loader = new ArrayLoader($config);
 
         $this->state_machine = new StateMachine($this->order);
 
@@ -266,20 +222,5 @@ class Cart implements CartInterface
     public function getStateMachine()
     {
         return $this->state_machine;
-    }
-
-    /**
-     * Checks the balance of Order after a `pay` transition.
-     * If balance is greater than zero then rollback to `checkout` state to
-     * fullfil the payment of the Order.
-     *
-     * @access public
-     * @return void
-     */
-    public function rollbackPayment()
-    {
-        if ($this->getOrder()->needsPayment()) {
-            $this->processTo('partial_pay');
-        }
     }
 }
