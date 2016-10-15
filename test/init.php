e1129741fe2d8f9e6cfc4d50ec0edbd4c6ab95b9
akDeveloper <andreas.kollaros@gmail.com>
2016-10-16 00:47:03 +0300

    remove unused test files

--- test/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/FixtureLoader.php';
-require_once __DIR__ . '/Hydrator.php';
-require_once __DIR__ . '/Larium/RedirectProvider.php';
-require_once __DIR__ . '/Stub/Order.php';
-require_once __DIR__ . '/Larium/Helper.php';
